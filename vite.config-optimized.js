0246eed7a54d0148a691e79c006546bef2ffa847
Shawn-cyber666 <lix222324@gmail.com>
2026-03-13 12:26:42 +0800

    Add files via upload

---
+++ vite.config-optimized.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    sourcemap: false,
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    strictPort: false
+  }
+})
